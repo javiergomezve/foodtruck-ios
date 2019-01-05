13e297a0e38060a0241d804e1262bb4379c40665
Javier Gomez <javiergomezve@gmail.com>
2019-01-05 12:44:02 -0400

    Data models

--- FoodTruck/Model/FoodTruckReview.swift
+++ FoodTruck/Model/FoodTruckReview.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+
+struct FoodTruckReview {
+    
+    var id: String = ""
+    var title: String = ""
+    var text: String = ""
+    
+    static func parseReviewJSONData(data: Data) -> [FoodTruckReview] {
+        var foodTruckReviews = [FoodTruckReview]()
+        
+        do {
+            let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+
+            if let reviews = jsonResult as? [Dictionary<String, AnyObject>] {
+                for review in reviews {
+                    
+                    var newReview = FoodTruckReview()
+                    newReview.id = review["_id"] as! String
+                    newReview.title = review["title"] as! String
+                    newReview.text = review["text"] as! String
+                    
+                    foodTruckReviews.append(newReview)
+                }
+            }
+        } catch let err {
+            print(err)
+        }
+        return foodTruckReviews
+    }
+}
